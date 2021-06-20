a398cce6ec2ec0d84fa7b60162ec21214d7d7671
stevenmartincd2019 <48608370+stevenmartincd2019@users.noreply.github.com>
2021-06-20 15:08:45 -0500

    Clase conexion
    Clase conexion

--- src/main/java/config/Conexion.java
+++ src/main/java/config/Conexion.java
@@ -5,10 +5,83 @@
  */
 package config;
 
+import java.sql.*;
 /**
  *
  * @author Steven
  */
 public class Conexion {
     
+    /**
+     * We establish the connection with the database <b>customerdb</b>.
+     * Establecemos la conexión con la base de datos <b>customerdb</b>.
+     */
+    public void connectDatabase() {
+        try {
+            // We register the PostgreSQL driver
+            // Registramos el driver de PostgresSQL
+            try { 
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+            }
+            Connection connection = null;
+            // Database connect
+            // Conectamos con la base de datos
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://192.168.0.26:5432/customerdb",
+                    "xulescode", "xulescode");
+ 
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+        } catch (java.sql.SQLException sqle) {
+            System.out.println("Error: " + sqle);
+        }
+    }
+ 
+    /**
+     * Method to connect to the database by passing parameters.
+     * Método para establecer la conexión a la base de datos mediante el paso de parámetros.
+     * 
+     * @param host <code>String</code> host name or ip. Nombre del host o ip.
+     * @param port <code>String</code> listening database port. Puerto en el que escucha la base de datos.
+     * @param database <code>String</code> database name for the connection. Nombre de la base de datos para la conexión.
+     * @param user <code>String</code> user name. Nombre de usuario.
+     * @param password  <code>String</code> user password. Password del usuario.
+     */
+    public void connectDatabase(String host, String port, String database,
+            String user, String password) {
+        String url = "";
+        try {
+            // We register the PostgreSQL driver
+            // Registramos el driver de PostgresSQL
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+            }
+            Connection connection = null;
+            url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
+            // Database connect
+            // Conectamos con la base de datos
+            connection = DriverManager.getConnection(
+                    url,
+                    user, password);           
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+        } catch (java.sql.SQLException sqle) { 
+            System.out.println("Error al conectar con la base de datos de PostgreSQL (" + url + "): " + sqle);
+        }
+    }
+ 
+    /**
+     * Testing Java PostgreSQL connection with host and port
+     * Probando la conexión en Java a PostgreSQL especificando el host y el puerto.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Conexion conexion = new Conexion();
+        conexion.connectDatabase();
+        conexion.connectDatabase("192.168.0.26", "5432", "customerdb","xulescode", "xulescode");
+    }
 }
